d204ccbb8828c088d5dd3a9b520d977921345e3a
Kirsten Greed <kirsteng@jobtalk.com.au>
2021-12-14 08:09:58 +1100

    more

---
+++ Dtg.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using Dtg.Module.BusinessObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Dtg.Blazor.Server {
+    public partial class DtgBlazorApplication : BlazorApplication {
+        public DtgBlazorApplication() {
+            InitializeComponent();
+        }
+        protected override void OnSetupStarted() {
+            base.OnSetupStarted();
+            IConfiguration configuration = ServiceProvider.GetRequiredService<IConfiguration>();
+            if(configuration.GetConnectionString("ConnectionString") != null) {
+                ConnectionString = configuration.GetConnectionString("ConnectionString");
+            }
+#if EASYTEST
+            if(configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                ConnectionString = configuration.GetConnectionString("EasyTestConnectionString");
+            }
+#endif
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            IDbContextFactory<DtgEFCoreDbContext> dbFactory = ServiceProvider.GetService<IDbContextFactory<DtgEFCoreDbContext>>();
+            SecuredEFCoreObjectSpaceProvider efCoreObjectSpaceProvider = new SecuredEFCoreObjectSpaceProvider((ISelectDataSecurityProvider)Security, dbFactory, TypesInfo);
+            args.ObjectSpaceProviders.Add(efCoreObjectSpaceProvider);
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+        private void DtgBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+                string message = "The application cannot connect to the specified database, " +
+                    "because the database doesn't exist, its version is older " +
+                    "than that of the application or its schema does not match " +
+                    "the ORM data model structure. To avoid this error, use one " +
+                    "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+    }
+}
---
+++ Dtg.Module/BusinessObjects/DtgDbContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.Persistent.Base;
+namespace Dtg.Module.BusinessObjects {
+    // This code allows our Model Editor to get relevant EF Core metadata at design time.
+    // For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+	public class DtgContextInitializer : DbContextTypesInfoInitializerBase {
+		protected override DbContext CreateDbContext() {
+			var optionsBuilder = new DbContextOptionsBuilder<DtgEFCoreDbContext>()
+                .UseSqlServer(@";");
+            return new DtgEFCoreDbContext(optionsBuilder.Options);
+		}
+	}
+	//This factory creates DbContext for design-time services. For example, it is required for database migration.
+	public class DtgDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DtgEFCoreDbContext> {
+		public DtgEFCoreDbContext CreateDbContext(string[] args) {
+			throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+			//var optionsBuilder = new DbContextOptionsBuilder<DtgEFCoreDbContext>();
+			//optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Dtg");
+			//return new DtgEFCoreDbContext(optionsBuilder.Options);
+		}
+	}
+	[TypesInfoInitializer(typeof(DtgContextInitializer))]
+	public class DtgEFCoreDbContext : DbContext {
+		public DtgEFCoreDbContext(DbContextOptions<DtgEFCoreDbContext> options) : base(options) {
+		}
+		public DbSet<ModuleInfo> ModulesInfo { get; set; }
+		public DbSet<ModelDifference> ModelDifferences { get; set; }
+		public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	    public DbSet<PermissionPolicyRole> Roles { get; set; }
+	    public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+
+        public DbSet<Guru> Gurus { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        public DbSet<Attribute> Attributes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Dtg.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+                b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+            });
+        }
+	}
+    [NavigationItem("Main")]
+	[Table("Gurus")]
+    public class Guru
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+    [NavigationItem("Main")]
+    [Table("Ratings")]
+
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        [Required]
+        public virtual User User { get; set; }
+
+        public decimal Score { get; set; }
+
+        public int AttributeId { get; set; }
+        [ForeignKey("AttributeId")]
+        [Required]
+        public virtual Attribute Attribute { get; set; }
+
+        public DateTime ScoredAt { get; set; }
+
+
+    }
+    [NavigationItem("Main")]
+    [Table("Attributes")]
+    public class Attribute
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class User : ApplicationUser
+    {
+        public string DiscordName { get; set; }
+    }
+}
