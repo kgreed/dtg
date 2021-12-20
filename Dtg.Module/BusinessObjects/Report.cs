cfb10ecc13ed30f64d2939a966a0cd13776eaa5c
Kirsten Greed <kirsteng@jobtalk.com.au>
2021-12-20 16:53:33 +1100

    git ignore

--- Dtg.Module/BusinessObjects/Report.cs
+++ Dtg.Module/BusinessObjects/Report.cs
@@ -1,9 +1,9 @@
-﻿using DevExpress.Persistent.Base;
-using DevExpress.Persistent.BaseImpl.EF;
-namespace Dtg.Module.BusinessObjects
-{
-    [NavigationItem("Main")]
-    public class Report :  ReportDataV2
-    {
-    }
-}
+﻿//using DevExpress.Persistent.Base;
+//using DevExpress.Persistent.BaseImpl.EF;
+//namespace Dtg.Module.BusinessObjects
+//{
+//    [NavigationItem("Main")]
+//    public class Report :  ReportDataV2
+//    {
+//    }
+//}
